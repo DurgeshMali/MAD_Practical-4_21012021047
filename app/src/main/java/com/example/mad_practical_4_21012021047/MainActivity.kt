9442dac037802aad7748f964972c45074c061dc7
student <student@gnu.ac.in>
2023-08-09 08:56:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_practical_4_21012021047/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.mad_practical_4_21012021047
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var login_button = findViewById<View>(R.id.button)
+        login_button.setOnClickListener {
+            intent = Intent(this, loginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
